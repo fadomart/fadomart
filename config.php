c1ad4b184f85d911586076af105e1cfdbb100ae4
fadomart <114821483+fadomart@users.noreply.github.com>
2022-10-01 22:55:40 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://fadomart.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://fadomart.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/wepropwz/public_html/catalog/');
+define('DIR_SYSTEM', '/home/wepropwz/public_html/system/');
+define('DIR_IMAGE', '/home/wepropwz/public_html/image/');
+define('DIR_STORAGE', '/home/wepropwz/public_html/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wepropwz_admin');
+define('DB_PASSWORD', 'Adminwepro');
+define('DB_DATABASE', 'wepropwz_storezo');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
